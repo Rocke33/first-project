be1cfb26aea760421042935cee584fa47eb178eb
Rocke33 <amirocke33@gmail.com>
2025-08-21 16:50:07 +0600

    Code forces
    800 Rating

---
+++ A_Maximum_GCD.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void rocke(){
+    int n;
+    cin>>n;
+    cout<<n/2<<endl;
+}
+int main(){
+   int t;
+   cin>>t;
+   while(t--){
+     rocke();
+   }
+    return 0;
+}
