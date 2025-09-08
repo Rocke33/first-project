31f51888081790b38d38580482a240ea50cab4c7
Rocke33 <amirocke33@gmail.com>
2025-09-09 03:46:20 +0600

    codeforeces problem
    800 Rating

---
+++ A_Easy_Problem.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rocke() {
+    int n;cin>>n;
+    cout<<n-1<<endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        rocke();
+    }
+    return 0;
+}
