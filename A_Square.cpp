8690061bdb6631f7f512febe47efd213e2890130
Rocke33 <amirocke33@gmail.com>
2025-10-27 00:28:05 +0600

    codeforces problem
    800 Rating

---
+++ A_Square.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rocke() {
+    int x,y;
+    vector<int>v;
+    for(int i=0;i<4;i++){
+        cin>>x>>y;
+        v.push_back(x);
+    }
+    int a= *max_element(v.begin(),v.end())- *min_element(v.begin(),v.end());
+    cout<<a*a<<endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        rocke();
+    }
+    return 0;
+}
