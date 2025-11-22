2ee45a4abf1f83a0acedb14a247da0db83a397dd
Rocke33 <amirocke33@gmail.com>
2025-11-22 11:53:27 +0600

    codeforces problem
    800 Rating

---
+++ A_LCM_Problem.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rocke() {
+long long int l,r;cin>>l>>r;
+  if(2*l<=r){
+    cout<<l<<" "<<2*l<<endl;
+  }
+  else {
+    cout<<"-1 -1"<<endl;
+  }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        rocke();
+    }
+    return 0;
+}
