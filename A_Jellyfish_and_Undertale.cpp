2d3038a89473c3b464e41f838a63fafd5c95364f
Rocke33 <amirocke33@gmail.com>
2025-11-24 23:03:36 +0600

    ggj
    hgjhf

--- A_Jellyfish_and_Undertale.cpp
+++ A_Jellyfish_and_Undertale.cpp
@@ -4,14 +4,16 @@ using namespace std;
 void rocke() {
     int a, b, n; cin >> a >> b >> n;
     vector<int>v(n);
-    int sum=0;
+    int sum=0;int time=b;
     for(int i=0;i<n;i++){
         cin>>v[i];
-        sum+=v[i];
     }
-    while(b--){
-        
-    }
+    sort(v.rbegin(),v.rend());
+    for(int i=1;i<n;i++){
+        time+=min(v[i],a-b);
+        }
+    
+    cout<<time<<endl;
 }
 
 int main() {
