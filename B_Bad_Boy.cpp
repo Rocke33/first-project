acd42c67bbea77f11f92af9b399fac449673613d
Rocke33 <amirocke33@gmail.com>
2025-12-11 12:44:03 +0600

    codeforces
    900 Rating

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rocke() {
+  int row,col,i,j; cin>>row>>col>>i>>j;
+  cout<<1<<" "<<1<<" "<<row<<" "<<col<<endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        rocke();
+    }
+    return 0;
+}
